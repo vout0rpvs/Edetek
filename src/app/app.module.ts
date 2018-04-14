c6d5c4ca6916e816c16f09ab78aa72ecf192e750
Oleh Fedoryhyn <vout0rpvs@gmail.com>
2018-04-14 18:46:26 +0300

    Edetek Challenge Adding existing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { MaterialModule } from './material.module';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './/app-routing.module';
+import { DepartmentsService } from './departments/departments.service';
+import { EmployeesService } from './employees/employees.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    DepartmentsService,
+    EmployeesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
